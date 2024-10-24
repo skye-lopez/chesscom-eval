b18e66563696c83a65ba9405b636dd1c78da8c5d
skye-lopez <sky.lopez.dev@gmail.com>
2024-10-23 19:33:13 -0700

    cleared some of the initial templating and added basic style setup

---
+++ src/styles/global.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        font-family: 'Ubuntu', sans-serif;
+    }
+    
+    #root {
+        margin: 0 auto;
+    }
+`;
